4e884ce43975fb857138767185f2541b243afbdd
David Refoua <David@Refoua.me>
2018-02-16 22:06:39 +0330

    Added License clause

--- google-restore-viewimage-button.user.js
+++ google-restore-viewimage-button.user.js
@@ -9,7 +9,8 @@
 // @run-at         document-start
 // @updateURL      https://raw.githubusercontent.com/DRSDavidSoft/user-scripts/master/google-restore-viewimage-button.user.js
 // @downloadURL    https://raw.githubusercontent.com/DRSDavidSoft/user-scripts/master/google-restore-viewimage-button.user.js
-// @version        0.1.1-beta
+// @version        0.1.2-beta
+// @license        MIT License
 // ==/UserScript==
 
 var resetViewBtn = (function() {
